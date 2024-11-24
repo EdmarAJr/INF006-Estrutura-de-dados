5ff698562293dee6c87c672346dde4e05f7a41f4
Edmar Amorim <trovador@hotmail.com.br>
2024-11-24 14:06:00 -0300

    REFACTOR: refatoração da lógica, tratamento de entrada e saída de dados

--- Lista 0/RL0Q2.c
+++ Lista 0/RL0Q2.c
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
 #define MAX_ITEMS 100
 #define MAX_STR_LEN 50
@@ -12,12 +12,10 @@ typedef struct {
     float y;
 } Ponto;
 
-// Função para calcular a distância euclidiana
 float distancia(Ponto p) {
     return sqrt(p.x * p.x + p.y * p.y);
 }
 
-// Função para ordenar strings
 void ordenarStrings(char strings[][MAX_STR_LEN], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -31,7 +29,18 @@ void ordenarStrings(char strings[][MAX_STR_LEN], int n) {
     }
 }
 
-// Função para ordenar inteiros ou reais
+void ordenarIntegers(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 void ordenarFloats(float arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -44,7 +53,6 @@ void ordenarFloats(float arr[], int n) {
     }
 }
 
-// Função para ordenar pontos pela distância
 void ordenarPontos(Ponto pontos[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -58,32 +66,39 @@ void ordenarPontos(Ponto pontos[], int n) {
 }
 
 int main() {
-    FILE *entrada = fopen("L0Q2.in", "r");
-    FILE *saida = fopen("L0Q2.out", "w");
+    FILE *input = fopen("L0Q2.in", "r");
+    FILE *output = fopen("L0Q2.out", "w");
 
-    if (!entrada || !saida) {
+    if (!input || !output) {
         printf("Erro ao abrir os arquivos.\n");
         return 1;
     }
 
-    char line[1000];
-    while (fgets(line, sizeof(line), entrada)) {
+    char line[255];
+    while (fgets(line, sizeof(line), input)) {
         char strings[MAX_ITEMS][MAX_STR_LEN];
         float reais[MAX_ITEMS];
         int inteiros[MAX_ITEMS];
         Ponto pontos[MAX_ITEMS];
         int countStr = 0, countReais = 0, countInt = 0, countPontos = 0;
 
-        char *token = strtok(line, " ");
+        char* token = strtok(line, " ");
+
         while (token != NULL) {
-            if (sscanf(token, "(%f,%f)", &pontos[countPontos].x, &pontos[countPontos].y) == 2) {
-                countPontos++;
-            } else if (sscanf(token, "%f", &reais[countReais]) == 1) {
-                if (strchr(token, '.')) { // Diferenciar entre float e int
-                    countReais++;
-                } else {
-                    inteiros[countInt++] = (int) reais[countReais];
-                    countReais--;
+            if (isdigit(token[0]) || token[0] == '-' || token[0] == '(') {
+                if (strchr(token, '(') != NULL) {  
+                    sscanf(token, "(%f,%f)", &pontos[countPontos].x, &pontos[countPontos].y);
+                    countPontos++;
+                } else { 
+                    char *end;
+                    float val = strtof(token, &end);
+                    if (*end == '\0' || (*end == 'f' && *(end + 1) == '\0')) {  
+                        if (strchr(token, '.') != NULL) {  
+                            reais[countReais++] = val;
+                        } else {  
+                            inteiros[countInt++] = (int)val;
+                        }
+                    }
                 }
             } else {
                 strcpy(strings[countStr++], token);
@@ -91,114 +106,34 @@ int main() {
             token = strtok(NULL, " ");
         }
 
-        // Ordenar os dados
         ordenarStrings(strings, countStr);
-        ordenarFloats((float *)inteiros, countInt);
+        ordenarIntegers(inteiros, countInt);
         ordenarFloats(reais, countReais);
         ordenarPontos(pontos, countPontos);
 
-        // Escrever no arquivo de saída
-        fprintf(saida, "str:");
-        for (int i = 0; i < countStr; i++) fprintf(saida, " %s", strings[i]);
-        fprintf(saida, " int:");
-        for (int i = 0; i < countInt; i++) fprintf(saida, " %d", inteiros[i]);
-        fprintf(saida, " float:");
-        for (int i = 0; i < countReais; i++) fprintf(saida, " %.2f", reais[i]);
-        fprintf(saida, " p:");
-        for (int i = 0; i < countPontos; i++) fprintf(saida, " (%.2f,%.2f)", pontos[i].x, pontos[i].y);
-        fprintf(saida, "\n");
+        fprintf(output, "str:");
+        for (int i = 0; i < countStr; i++) fprintf(output, " %s", strings[i]);
+        fprintf(output, " int:");
+        for (int i = 0; i < countInt; i++) fprintf(output, " %d", inteiros[i]);
+        fprintf(output, " float:");
+        for (int i = 0; i < countReais; i++) fprintf(output, " %.2f", reais[i]);
+        fprintf(output, " p:");
+        for (int i = 0; i < countPontos; i++) {            
+            if (pontos[i].x == (int)pontos[i].x && pontos[i].y == (int)pontos[i].y) {
+                fprintf(output, "(%d, %d) ", (int)pontos[i].x, (int)pontos[i].y);
+            } else if (pontos[i].x != (int)pontos[i].x && pontos[i].y == (float)pontos[i].y) {
+                fprintf(output,"(%d, %.2f) ", (int)pontos[i].x, pontos[i].y);
+            } else if (pontos[i].x == (float)pontos[i].x && pontos[i].y == (int)pontos[i].y) {
+                fprintf(output,"(%.2f, %d) ", pontos[i].x, (int)pontos[i].y);
+            } else {
+                fprintf(output,"(%.2f, %.2f) ", pontos[i].x, pontos[i].y);
+            }
+        }
+        fprintf(output, "\n");
     }
 
-    fclose(entrada);
-    fclose(saida);
+    fclose(input);
+    fclose(output);
 
     return 0;
 }
-=======
-#include <stdio.h>
-#include <math.h>
-#define TAM 5
-
-
-typedef struct ponto {
-    float x;
-    float y;
-    } Ponto;
-
-typedef struct pessoa {
-        char nome[50];
-        float reais[50];
-        int inteiros[50];
-        Ponto coord[50];
-    } Pessoa;
-
-
-void selectionSortReais(Pessoa *A, int tam) {	
- 	for (int i = 0; i < tam; i++) {
- 		int i_menor = i;
- 		for (int j = i + 1; j < tam; j++){
- 			if (A[j].reais[j] < A[i_menor].reais[i_menor])
- 				i_menor = j;
- 		}
-     	int aux = A[i].reais[i];
-     	A[i].reais[i] = A[i_menor].reais[i_menor];
-     	A[i_menor].reais[i_menor] = aux;
- 	}		
- }
- 
-void selectionSortInteiros(Pessoa *A, int tam) {	
- 	for (int i = 0; i < tam; i++) {
- 		int i_menor = i;
- 		for (int j = i + 1; j < tam; j++){
- 			if (A[j].inteiros[j] < A[i_menor].inteiros[i_menor])
- 				i_menor = j;
- 		}
-     	int aux = A[i].inteiros[i];
-     	A[i].inteiros[i] = A[i_menor].inteiros[i_menor];
-     	A[i_menor].inteiros[i_menor] = aux;
- 	}		
- }
- 
-void printStr(Pessoa *lista){
-    for(int i=0; i<5; i++){
-        printf("str: %s ", lista[i].nome);
-    }
-}
-
-void printFloat(Pessoa *lista){
-    selectionSortReais(lista, 5);
-    for(int i=0; i<5; i++){
-        printf("float: %.2f ", lista[i].reais[i]);
-    }
-}
-
-void printInt(Pessoa *lista){
-    selectionSortInteiros(lista, 5);
-    for(int i=0; i<5; i++){
-        printf("int: %d ", lista[i].inteiros[i]);
-    }
-}
-
-
-
-int main(){
-    //Pessoa lista[50] = {"Edmar", {3.15}, {}, {}};
-    Pessoa lista[50];
-    for(int i=0; i<5; i++){
-        fgets(lista[i].nome, 50, stdin);
-        //getchar();
-        scanf("%f", &lista[i].reais[i]);
-        scanf("%d", &lista[i].inteiros[i]);
-        scanf("%f", &lista[i].coord[i].x);
-        scanf("%f", &lista[i].coord[i].y);
-        
-    }
-    
-
-
-    printStr(lista);
-    printFloat(lista);
-    printInt(lista);
-    return 0;
-}
->>>>>>> 947c55ba5bde9a8bad2147047b1fd4cc81d9c853
