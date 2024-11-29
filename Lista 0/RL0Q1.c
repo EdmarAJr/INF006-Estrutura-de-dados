c3af90d3121fcfdd84ccd0b560b11cec161cab69
Edmar Amorim <trovador@hotmail.com.br>
2024-11-28 22:14:08 -0300

    Update: RL0Q1.c

--- Lista 0/RL0Q1.c
+++ Lista 0/RL0Q1.c
@@ -1,13 +1,47 @@
+/*************************************** 
+* Equipe: Deivisson Vinicius França de Jesus
+*         Edmar Amorim dos Santos Junior
+* 
+***************************************/
+              
+/**
+* 1. Considere as seguintes entrada e saída abaixo:
+*                     L0Q1.in
+*       points (−2,−1) (4 ,2) (4,0) (2,2) (8,6)
+
+*                     L0Q1.out
+*        points (−2,−1) (2 ,2) (4,0) (4,2) (8,6) distance 18.75 shortcut 12.21
+
+ * Sobre a entrada
+ * A cada linha um points marca o início de uma lista de pontos no espaçoo bidimensional; veja a figura abaixo.
+
+ *  Sobre a saída
+ * Na saída é preciso ordenar todos os pontos points conforme suas distâncias Euclidianas em relação à origem, ou seja, em relação ao ponto (0,0).
+ * Em seguida deve-se exibir a distância total Euclidiana distance considerando os pontos na mesma ordem em que aparecem na entrada.
+ * Por fim, deve-se exibir a distˆancia total Euclidiana shortcut entre o primeiro e último ponto na mesma ordem em que aparecem na entrada.
+
+ ** Observações
+ * Não faça Ctrl+C/Ctrl+V deste arquivo PDF no seu arquivo de entrada pois podem ocorrer erros.
+ * Cada ponto aparece separado por apenas um espaço em branco dos demais e entre (), números com parte decimal são representados usando o ponto final e não a vírgula.
+ * Sua resposta deve estar em um único arquivo chamado RL0Q1.[java, py, js, c, cpp, etc]
+ * Você deve ler o arquivo L0Q1.in e escrever o arquivo L0Q1.out no mesmo diretório onde estiver rodando seu código RL0Q1.[java, py, js, c, cpp, etc]
+ * Não deve ser usado nenhum recurso de ordena¸ c˜ao da linguagem de programação de sua escolha
+ * Pode e vai haver mais de uma linha na estrada
+ * O número de linhas na entrada deve ser o mesmo da saída, ou seja cada linha da entrada produzirá uma linha na saída
+ * Os pontos podem ter coordenadas reais, ou seja o ponto (13.34,10.2) é um ponto válido para a entrada
+ * não existem dois espaços em branco seguidos
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-#define MAX_POINTS 100
+#define MAX_POINTS 500
 typedef struct ponto {
-        float x;
-        float y;
-    } Ponto;
+    float x;
+    float y;
+} Ponto;
 
 void selectionSort(Ponto *A, int tam) {
     for (int i = 0; i < tam - 1; i++) {
@@ -41,11 +75,11 @@ void print(Ponto *A, int tam) {
 }    
 
 int main() {
+    
     FILE *input, *output;
-    //char line[1000];
     input = fopen("L0Q1.in", "r");
     output = fopen("L0Q1.out", "w");
-    char line[255];
+    char line[1000]; 
 
     if (!input || !output) {
         printf("Erro ao abrir os arquivos.\n");
@@ -63,10 +97,10 @@ int main() {
         }
 
         while (token != NULL) {
-            Ponto p;
-            if (sscanf(token, "(%f,%f)", &p.x, &p.y) == 2) {
-                arrayPontos[count] = p;
-                arrayPontosOrdenados[count] = p;
+            Ponto ponto;
+            if (sscanf(token, "(%f,%f)", &ponto.x, &ponto.y) == 2) {
+                arrayPontos[count] = ponto;
+                arrayPontosOrdenados[count] = ponto;
                 count++;
             }
             token = strtok(NULL, " ");
@@ -80,12 +114,12 @@ int main() {
         }
         
         shortcut = sqrt(pow((arrayPontos[0].x  - arrayPontos[count-1].x), 2) + pow((arrayPontos[0].y  - arrayPontos[count-1].y), 2));
-        
+
         selectionSort(arrayPontosOrdenados, count);
     
-        print(arrayPontosOrdenados, count);
-        printf("distance %.2f ", distance);
-        printf("shortcut %.2f ", shortcut);    
+        // print(arrayPontosOrdenados, count);
+        // printf("distance %.2f ", distance);
+        // printf("shortcut %.2f ", shortcut);    
 
         fprintf(output, "points ");
         for (int i = 0; i < count; i++) {    
